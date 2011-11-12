f8f0a6849111988d026745ce5269d31188a9328d
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-12 11:06:07 +0800

    n

--- continuous_num.cc
+++ continuous_num.cc
@@ -7,8 +7,9 @@
 
 using namespace std;
 
-int GetSubString(char *strSource, char *strResult) 
+char* GetSubString(char *strSource, int len) 
 {
+  char * strResult=new char[len];
   int iHead=0, iMax=0;
   int Index=0, iLen=0,nav=0; 
   while(strSource[Index])
@@ -31,12 +32,12 @@ int GetSubString(char *strSource, char *strResult)
   for(int i=0 ; i< iMax; i++) // 将原字符串中最长的连续数字串赋值给结果串
     strResult[i] = strSource[iHead++];
   strResult[iMax]='\0';
-  return iMax;     // 返回连续数字的最大长度
+  return strResult;     // 返回连续数字的最大长度
 }
 
 int main() 
 {
   char strSource[]="ads3sl456789DF3456ld345AA";
-  char strResult[sizeof(strSource)];
-  printf("Len=%d, strResult=%s \nstrSource=%s\n",GetSubString(strSource, strResult), strResult, strSource);
+  char * result=GetSubString(strSource,sizeof(strSource)/sizeof(char));
+  cout<<result<<endl;
 }
