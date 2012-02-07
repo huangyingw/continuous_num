44dbe4f0b8e604a9b3ed903568ef93474d844d47
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-07 10:46:34 +0800

    n

--- src/continuous_num.java
+++ src/continuous_num.java
@@ -1,21 +1,21 @@
 public class continuous_num {
 	static char[] GetSubString(char[] strSource) {
 		int iHead = 0, iMax = 0;
-		int Index = 0, iLen = 0, nav = 0;
-		while (Index < strSource.length) {
+		int index = 1, iLen = 0, nav = 0;
+		while (index < strSource.length) {
 			iLen = 1;
-			nav = Index;
+			nav = index;
 			while (nav < strSource.length && strSource[nav] >= '0'
-					&& strSource[nav] <= '9' && strSource[nav - 1] > '0'
+					&& strSource[nav] <= '9' && strSource[nav - 1] >= '0'
 					&& strSource[nav] == strSource[nav - 1] + 1) {
 				iLen++; // 连续数字的长度增1
 				nav++;
 			}
 			if (iMax < iLen) {
 				iMax = iLen;
-				iHead = Index - 1;
+				iHead = index - 1;
 			}
-			Index++;
+			index++;
 		}
 		char[] strResult = new char[iMax];
 		for (int i = 0; i < iMax; i++) {
@@ -26,13 +26,16 @@ public class continuous_num {
 	}
 
 	public static void main(String arg[]) {
-		char[] strSource = "ads3sl456789DF3456ld345AA".toCharArray();
+		char[] strSource = "ads3s23456789l456789DF".toCharArray();
 		char[] result = GetSubString(strSource);
 		System.out.println(result);
 
-		char strSource1[] = "ads3sl456789DF23456789".toCharArray();
-		result = GetSubString(strSource1);
+		strSource = "ads3sl456789DF23456789".toCharArray();
+		result = GetSubString(strSource);
 		System.out.println(result);
 
+		strSource = "23456789ads3sl456789DF".toCharArray();
+		result = GetSubString(strSource);
+		System.out.println(result);
 	}
 }
